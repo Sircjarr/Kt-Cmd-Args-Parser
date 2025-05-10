e2b0c5d72af65a3b30a49c425248cd98fe255391
Cliff Jarrell <you@example.com>
2025-05-10 08:31:13 -0500

    clean help printer

--- src/main/kotlin/help/CmdArgsParserHelpPrinter.kt
+++ src/main/kotlin/help/CmdArgsParserHelpPrinter.kt
@@ -41,11 +41,11 @@ internal object CmdArgsParserHelpPrinter {
     }
 
     private fun buildSubcommandTuples(subcommands: MutableMap<String, Subcommand<Any?>>, tuples: MutableList<List<Any>>) {
-        if (subcommands.isNotEmpty()) {
-            tuples.add(listOf("Subcommands:"))
-            subcommands.forEach { k, v ->
-                tuples.add(listOf(k))
-            }
+        if (subcommands.isEmpty()) return
+
+        tuples.add(listOf("Subcommands:"))
+        subcommands.forEach { k, v ->
+            tuples.add(listOf(k))
         }
     }
 
@@ -96,47 +96,47 @@ internal object CmdArgsParserHelpPrinter {
     }
 
     private fun buildRequiredTuples(required: List<CmdArgNonNull.KeyValue<Any>>, tuples: MutableList<List<Any>>) {
-        if (required.isNotEmpty()) {
-            tuples.add(listOf("Required args:"))
-            for (r in required) {
-                val tuple = buildList {
-                    add(r.keys.joinToString(", ") { "$it ${r.valueLabel}" })
-                    add(CHAR_TABLE_SEPARATOR)
-                    add(r.hint)
-                    if (r.default != null) {
-                        add(defaultValueString(r.default))
-                    }
-                }
+        if (required.isEmpty()) return
 
-                tuples.add(tuple)
-
-                val mapping = when (r) {
-                    is CmdArgNonNull.KeyValue.Mapped -> r.map
-                    else -> null
-                }
-
-                if (mapping?.entries?.isNotEmpty() == true) {
-                    val choices = mapping.entries.joinToString(",") { it.key }
-                    tuples.add(listOf("\t${r.valueLabel}={$choices}"))
+        tuples.add(listOf("Required args:"))
+        for (r in required) {
+            val tuple = buildList {
+                add(r.keys.joinToString(", ") { "$it ${r.valueLabel}" })
+                add(CHAR_TABLE_SEPARATOR)
+                add(r.hint)
+                if (r.default != null) {
+                    add(defaultValueString(r.default))
                 }
             }
-            tuples.add(emptyList<Unit>())
+
+            tuples.add(tuple)
+
+            val mapping = when (r) {
+                is CmdArgNonNull.KeyValue.Mapped -> r.map
+                else -> null
+            }
+
+            if (mapping?.entries?.isNotEmpty() == true) {
+                val choices = mapping.entries.joinToString(",") { it.key }
+                tuples.add(listOf("\t${r.valueLabel}={$choices}"))
+            }
         }
+        tuples.add(emptyList<Unit>())
     }
 
     private fun buildPositionalTuples(
         positionals: List<CmdArgNonNull.Positional<Any>>,
         tuples: MutableList<List<Any>>
     ) {
-        if (positionals.isNotEmpty()) {
-            tuples.add(listOf("Positional args:"))
-            for (p in positionals) {
-                val label = p.valueLabel
-                val hint = p.hint
-                tuples.add(listOf(label, CHAR_TABLE_SEPARATOR, hint))
-            }
-            tuples.add(emptyList<Unit>())
+        if (positionals.isEmpty()) return
+
+        tuples.add(listOf("Positional args:"))
+        for (p in positionals) {
+            val label = p.valueLabel
+            val hint = p.hint
+            tuples.add(listOf(label, CHAR_TABLE_SEPARATOR, hint))
         }
+        tuples.add(emptyList<Unit>())
     }
 
     private fun buildOptionalTuples(
@@ -144,62 +144,62 @@ internal object CmdArgsParserHelpPrinter {
         optDefaults: List<CmdArgNonNull.KeyValue<Any>>,
         tuples: MutableList<List<Any>>
     ) {
-        if (opts.isNotEmpty() || optDefaults.isNotEmpty()) {
-            tuples.add(listOf("Optional args:"))
-            for (opt in opts) {
-                val tuple = buildList {
-                    add(opt.keys.joinToString(", ") { "$it ${opt.valueLabel}" })
-                    add(CHAR_TABLE_SEPARATOR)
-                    add(opt.hint)
-                }
+        if (opts.isEmpty() && optDefaults.isEmpty()) return
 
-                tuples.add(tuple)
-
-                val mapping = when (opt) {
-                    is CmdArgNullable.KeyValue.Mapped -> opt.map
-                    else -> null
-                }
-
-                if (mapping?.entries?.isNotEmpty() == true) {
-                    val choices = mapping.entries.joinToString(",") { it.key }
-                    tuples.add(listOf("\t${opt.valueLabel}={$choices}"))
-                }
+        tuples.add(listOf("Optional args:"))
+        for (opt in opts) {
+            val tuple = buildList {
+                add(opt.keys.joinToString(", ") { "$it ${opt.valueLabel}" })
+                add(CHAR_TABLE_SEPARATOR)
+                add(opt.hint)
             }
 
-            for (opt in optDefaults) {
-                val tuple = buildList {
-                    add(opt.keys.joinToString(", ") { "$it ${opt.valueLabel}" })
-                    add(CHAR_TABLE_SEPARATOR)
-                    add(opt.hint)
+            tuples.add(tuple)
 
-                    val default = when (opt) {
-                        is CmdArgNonNull.KeyValue.Single -> opt.default
-                        is CmdArgNonNull.KeyValue.Mapped -> {
-                            opt.default?.let {
-                                findKeyByValue(opt.map, it)
-                            }
+            val mapping = when (opt) {
+                is CmdArgNullable.KeyValue.Mapped -> opt.map
+                else -> null
+            }
+
+            if (mapping?.entries?.isNotEmpty() == true) {
+                val choices = mapping.entries.joinToString(",") { it.key }
+                tuples.add(listOf("\t${opt.valueLabel}={$choices}"))
+            }
+        }
+
+        for (opt in optDefaults) {
+            val tuple = buildList {
+                add(opt.keys.joinToString(", ") { "$it ${opt.valueLabel}" })
+                add(CHAR_TABLE_SEPARATOR)
+                add(opt.hint)
+
+                val default = when (opt) {
+                    is CmdArgNonNull.KeyValue.Single -> opt.default
+                    is CmdArgNonNull.KeyValue.Mapped -> {
+                        opt.default?.let {
+                            findKeyByValue(opt.map, it)
                         }
                     }
-
-                    if (default != null) {
-                        add(defaultValueString(default))
-                    }
                 }
 
-                tuples.add(tuple)
-
-                val mapping = when (opt) {
-                    is CmdArgNonNull.KeyValue.Mapped -> opt.map
-                    else -> null
-                }
-
-                if (mapping?.entries?.isNotEmpty() == true) {
-                    val choices = mapping.entries.joinToString(",") { it.key }
-                    tuples.add(listOf("\t${opt.valueLabel}={$choices}"))
+                if (default != null) {
+                    add(defaultValueString(default))
                 }
             }
-            tuples.add(emptyList<Unit>())
+
+            tuples.add(tuple)
+
+            val mapping = when (opt) {
+                is CmdArgNonNull.KeyValue.Mapped -> opt.map
+                else -> null
+            }
+
+            if (mapping?.entries?.isNotEmpty() == true) {
+                val choices = mapping.entries.joinToString(",") { it.key }
+                tuples.add(listOf("\t${opt.valueLabel}={$choices}"))
+            }
         }
+        tuples.add(emptyList<Unit>())
     }
 
     private fun findKeyByValue(map: Map<String, Any>, value: Any): String {
@@ -216,15 +216,15 @@ internal object CmdArgsParserHelpPrinter {
         flags: List<CmdArgNonNull.Flag>,
         tuples: MutableList<List<Any>>
     ) {
-        if (flags.isNotEmpty()) {
-            tuples.add(listOf("Flag args:"))
-            for (flag in flags) {
-                val keys = flag.keys.joinToString(", ") { it }
-                val hint = flag.hint
-                tuples.add(listOf(keys, CHAR_TABLE_SEPARATOR, hint, defaultValueString(flag.default)))
-            }
-            tuples.add(emptyList<List<Unit>>())
+        if (flags.isEmpty()) return
+
+        tuples.add(listOf("Flag args:"))
+        for (flag in flags) {
+            val keys = flag.keys.joinToString(", ") { it }
+            val hint = flag.hint
+            tuples.add(listOf(keys, CHAR_TABLE_SEPARATOR, hint, defaultValueString(flag.default)))
         }
+        tuples.add(emptyList<List<Unit>>())
     }
 
     private fun defaultValueString(value: Any): String {
