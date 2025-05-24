3646699c84a5113fb1e51b02f36d27d9fc16e239
Cliff Jarrell <cjarrell222@gmail.com>
2025-05-23 20:01:48 -0500

    add GPL license header

--- src/test/kotlin/CmdArgsPositionalsTest.kt
+++ src/test/kotlin/CmdArgsPositionalsTest.kt
@@ -1,3 +1,19 @@
+// Kt-Cmd-Args-Parser: A library for parsing command-line arguments.
+// Copyright (C) 2025 Cliff Jarrell
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 import com.github.sircjarr.cmdargsparser.CmdArgsParser
 import com.github.sircjarr.cmdargsparser.exception.CmdArgsMalformedException
 import kotlin.test.Test
